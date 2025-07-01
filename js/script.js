51c702a6081100ff36cd724f0da267d17bcc2c30
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-07-01 20:09:07 +0200

    com7

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const linkSaldi = document.getElementById("hideSaldi");
 const linkSummer = document.getElementById("linkSummer");
 
+const buttons = document.querySelectorAll("main button, footer button");
+const linkButtons = document.querySelectorAll(".btn-info");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].style.fontWeight = "bold";
+}
+
+for (let i = 0; i < linkButtons.length; i++) {
+  linkButtons[i].style.fontWeight = "bold";
+}
+
 const hideS = function () {
   const sRow = document.getElementById("saldiRow");
   let sRowDisp = window.getComputedStyle(sRow);
@@ -23,3 +33,25 @@ const hideSummer = function () {
 
 linkSaldi.addEventListener("click", hideS);
 linkSummer.addEventListener("click", hideSummer);
+
+const navLinks = document.querySelectorAll("nav li a");
+
+navLinks[1].addEventListener("click", () => {
+  const SaldiDiv = document.getElementById("SaldiCont");
+  SaldiDiv.scrollIntoView();
+});
+
+navLinks[2].addEventListener("click", () => {
+  const SaldiDiv = document.getElementById("SummerCont");
+  SaldiDiv.scrollIntoView();
+});
+
+navLinks[3].addEventListener("click", () => {
+  const SaldiDiv = document.getElementById("MinuteCont");
+  SaldiDiv.scrollIntoView();
+});
+
+navLinks[4].addEventListener("click", () => {
+  const SaldiDiv = document.getElementById("RecenCont");
+  SaldiDiv.scrollIntoView();
+});
